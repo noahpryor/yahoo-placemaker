21d57f1d02134e16a19c7eea14cc256f7f38f96e
Kyle Decot <kyle.decot@me.com>
2012-02-25 00:02:38 -0500

    Using VCR again. We'll see if Travis complains

--- spec/yahoo_placemaker_spec.rb
+++ spec/yahoo_placemaker_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe Yahoo::Placemaker do
 
   it "determines we're talking about Columbus, Ohio" do
-  #VCR.use_cassette('columbus ohio') do
+  VCR.use_cassette('columbus ohio') do
       response = Yahoo::Placemaker.extract "columbus ohio"
       response.document.administrative_scope.name.should == "Columbus, OH, US"
-    #end
+    end
   end
 
   it "returns an administrative scope of 'United States' when given a string of text w/ two US locations" do
-  #    VCR.use_cassette('columbus ohio miami florida') do
+      VCR.use_cassette('columbus ohio miami florida') do
       response = Yahoo::Placemaker.extract "columbus ohio miami florida"
       response.document.administrative_scope.name.should == "United States"
-   # end
+    end
   end
 
   it "contains only one item in reference_list when given 'Atlanta Georgia'" do
-    #  VCR.use_cassette('atlanta_georgia') do
+    VCR.use_cassette('atlanta_georgia') do
         response = Yahoo::Placemaker.extract "Atlanta Georgia"
         response.document.reference_list.size.should == 1
-    #end
+    end
   end
 
 end
