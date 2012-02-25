63b6a071ade0403f68bc2d93dc241d4bfdff92ef
Kyle Decot <kyle.decot@me.com>
2012-02-24 22:09:51 -0500

    Don't try to assign things that aren't present in the json response!

--- lib/yahoo-placemaker/document.rb
+++ lib/yahoo-placemaker/document.rb
@@ -8,18 +8,36 @@ require 'yahoo-placemaker/place_details'
 class Yahoo::Placemaker::Document
   attr_accessor :administrative_scope, :geographic_scope, :local_scopes, :reference_list, :extents, :place_details
   def initialize(json)
-    @administrative_scope = json['administrativeScope']
-    @geographic_scope = Yahoo::Placemaker::GeographicScope.new(json['geographicScope'])
-    @administrative_scope = Yahoo::Placemaker::AdministrativeScope.new(json['administrativeScope'])
-    @localScopes = Array.new
-    json['localScopes'].each do |ls|
-      @localScopes << Yahoo::Placemaker::LocalScope.new(ls)
+
+    if json['administrativeScope']
+      @administrative_scope = json['administrativeScope']
+    end
+
+    if json['geographicScope']
+      @geographic_scope = Yahoo::Placemaker::GeographicScope.new(json['geographicScope'])
+    end
+
+    if json['administrativeScope']
+      @administrative_scope = Yahoo::Placemaker::AdministrativeScope.new(json['administrativeScope'])
+    end
+
+    if json['localScopes']
+      @localScopes = Array.new
+      json['localScopes'].each do |ls|
+        @localScopes << Yahoo::Placemaker::LocalScope.new(ls)
+      end
     end
     if json['placeDetails']
       @place_details = Yahoo::Placemaker::PlaceDetails.new json['placeDetails']
     end
-    @extents = Yahoo::Placemaker::Extents.new(json['extents'])
-    @reference_list = Yahoo::Placemaker::ReferenceList.new json['referenceList']
+
+    if json['extents']
+      @extents = Yahoo::Placemaker::Extents.new(json['extents'])
+    end
+
+    if json['referenceList']
+      @reference_list = Yahoo::Placemaker::ReferenceList.new json['referenceList']
+    end
 
   end
 end
